07ede441360eb7b61a314381d5d4abd1255a3b81
helenaliu00 <162529496+helenaliu00@users.noreply.github.com>
2024-04-15 19:02:17 +1000

    Add files via upload

---
+++ P5 sketch 2.js
@@ -0,0 +1,31 @@
+//New//
+
+var img, capture;
+function preload (){
+img=loadImage("Images/Cat_Element.png");
+}
+
+function setup() {
+  createCanvas (windowWidth, windowHeight);
+  //background (0, 0, 255);
+  imageMode(CENTER);
+capture=createCapture(VIDEO);
+capture.size(width,height);
+capture.hide();
+}
+
+
+function draw() {
+  let rx=random(width);
+  let ry=random(height);
+  let circlewidth=random(4,30);
+  //if (mouseIsPressed){
+    //image(capture, width/2, height/2, width, height);
+  let c=img2.get(rx,ry);
+  fill(c);
+  noStroke();
+  circle(rx,ry, circlewidth);
+    //else{
+//image (capture, width/2, height/2,);}
+//filter(POSTERIZE,2);
+}
